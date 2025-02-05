11d55f2c4e68d5d5172ca63e23e705963539920c
prachips02 <49784723+Techpro-Compsoft@users.noreply.github.com>
2025-02-05 13:04:17 +0530

    Alert code is updated

---
+++ test/AlertPage/AlertPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.DropdownPage
+{
+    public class AlertPage
+    {
+        private readonly IWebDriver driver;
+
+        public AlertPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement button => driver.FindElement(By.Id("alertButton"));
+
+        public void ClickButton()
+        {
+            ClickElement(button);
+            IAlert alert = driver.SwitchTo().Alert();
+           
+            Assert.AreEqual("You clicked a button" , alert.Text);
+            alert.Accept();
+        }
+
+        public void ClickElement(IWebElement ele)
+        {
+            ele.Click();
+        }
+    }
+}
---
+++ test/AlertTest/AlertTest.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test.DropdownPage;
+
+namespace test.DropdownTest
+{
+    public class AlertTest
+    {
+        IWebDriver driver;
+        AlertPage alertpage;
+
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://demoqa.com/alerts");
+            alertpage = new AlertPage(driver);
+
+        }
+
+        [Test]
+        public void test()
+        {
+            alertpage.ClickButton();
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
