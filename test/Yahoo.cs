f2a760a2d316634421fb687edeb79125d39c3f7e
prachips02 <49784723+Techpro-Compsoft@users.noreply.github.com>
2025-02-13 11:23:08 +0530

    json content

--- test/Yahoo.cs
+++ test/Yahoo.cs
@@ -1,7 +1,9 @@
-﻿using OpenQA.Selenium;
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +35,41 @@ namespace test
              var windowHandles = driver.WindowHandles;
             driver.SwitchTo().Window(windowHandles[1]);
             Assert.IsTrue(driver.Title.Contains("Selenium Webdriver"), "Its not locating");
+
+            //Get the Top 5 results 
+            var searchResults = driver.FindElements(By.CssSelector("h3 a"));
+            List<Dictionary<string, string>> topResults = new List<Dictionary<string, string>>();
+            for (int i = 0; i < Math.Min(5, searchResults.Count); i++)
+            {
+                string title = searchResults[i].Text;
+                string url = searchResults[i].GetAttribute("href");
+
+                if(!string.IsNullOrEmpty(title))
+                {
+                    topResults.Add(new Dictionary<string, string>
+                    {
+                    { "Title", title  },
+                    { "URL", url }
+
+                    });
+                }
+               
+            }
+
+            //Convert List to Json
+            string jsonOutput = JsonConvert.SerializeObject(topResults , Formatting.Indented);
+
+            //Save Json to file
+         
+            string Filepath = "YahooSearchResults.json";
+            File.WriteAllText(Filepath, jsonOutput);
+            Console.WriteLine("Top 5 search results saved to YahooSearchResults.json");
+
+            //Read and display Json Results
+            string jsonContent = File.ReadAllText(Filepath);
+            Console.WriteLine("\nJson File Content");
+            Console.WriteLine(jsonContent);
+
         }
         [TearDown]
         public void tearDown() 
